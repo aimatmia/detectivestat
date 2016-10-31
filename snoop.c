2dd4732597bf2ca1db689160d2f2ad4f37597d44
Mia <mobrien@stuy.edu>
2016-10-31 15:31:48 -0400

    Update snoop.c

--- snoop.c
+++ snoop.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+#include <stdmath.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <unistd.h>
+#include <time.h>
+
+
+int main(){ 
+  struct stat info;
+  
+}
